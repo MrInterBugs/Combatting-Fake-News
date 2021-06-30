96e9d24bf4bb892e46468a278bbb5d02cfdbab4c
Aedan Lawrence <Aedan@MrInterBugs.uk>
2021-06-30 08:38:38 +0100

    Added a table for storing news.

--- Python Flask Enviroment/database.py
+++ Python Flask Enviroment/database.py
@@ -16,3 +16,15 @@ class PrivateKey(db.Model):
 
     def __repr__(self):
         return self.private_key
+
+
+class NewsArticle(db.Model):
+    """This is the table which contains the keys for the publishers."""
+    id = db.Column(db.Integer, primary_key=True)
+    publisher = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(250), unique=False, nullable=False)
+    content = db.Column(db.String(5000), unique=False, nullable=False)
+
+    def __repr__(self):
+        return {'Publisher': self.publisher, 'Title': self.title, 'Author': self.author, 'Content': self.content}
