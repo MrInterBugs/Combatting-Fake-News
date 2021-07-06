a4fc13e1faba8ae3d56b52b6ee06b545741769cc
Aedan Lawrence <Aedan@MrInterBugs.uk>
2021-07-06 13:36:13 +0100

    Signs news from the Guardian using their API.
    Takes URL from user, finds the news that relates to that URL, signs it and sends it back to the end user. If it is not from the guardian then it will be invalid.

--- Python Flask Enviroment/app.py
+++ Python Flask Enviroment/app.py
@@ -1,41 +1,56 @@
 """Used to verify the authenticity of provided news to a source."""
-from cryptography.exceptions import InvalidSignature
+import json
+
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 from flask import Flask, request
-from database import PrivateKey
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Student/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class PrivateKey(db.Model):
+    """This is the table which contains the keys for the publishers."""
+    __tablename__ = 'publishers'
+    publisher = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    private_key = db.Column(db.String(2100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.private_key
+
+
+GUARDIAN_API_KEY = "e75ca83b-5396-4d64-85b5-598ba5eff24a"
 
 
 @app.route('/')
 def encrypted():
     """Returns the a json of the article and the included signature."""
-    article = request.args.get('article')
-    print(article)
-    return {'Signature': SIGNATURE.hex(),
-            'articleContent': ARTICLE_CONTENT,
-            'articleAuthor': ARTICLE_AUTHOR,
-            'articleTitle': ARTICLE_TITLE,
-            'articlePublisher': ARTICLE_PUBLISHER}
-
-
-@app.route('/verify')
-def decrypted():
-    """Quick example to make sure the the signature matches."""
     try:
-        public_key = PRIVATE_KEY.public_key()
-        public_key.verify(
-            SIGNATURE,
-            FULL_ARTICLE,
-            ec.ECDSA(hashes.SHA256())
-        )
-        return "Valid"
-    except InvalidSignature:
-        return "Invalid"
+        article = request.args.get('article')
+
+        if 'theguardian' in article:
+            response = urlopen('https://content.guardianapis.com/' + (
+                article.split(".com/", 1)[1]) + '?api-key=e75ca83b-5396-4d64-85b5-598ba5eff24a')
+
+            json_obj = json.loads(response.read().decode('utf-8'))
+            private_key = read_key("Guardian")
+            signature = sign_news(private_key, bytes(response))
+
+            return {'Signature': signature.hex(),
+                    'Article': json_obj}
+        else:
+            return {'Status': 'Untrusted Source'}
+    except HTTPError:
+        return {'Status': 'Something went wrong'}
 
 
 def sign_news(private_key, full_article):
@@ -50,15 +65,6 @@ def read_key(publisher):
     private_key = str(PrivateKey.query.filter_by(publisher=publisher).first()).encode()
     return load_pem_private_key(private_key, None, default_backend())
 
-
-ARTICLE_TITLE = "BREAKING NEWS: Royal Holloway is a University"
-ARTICLE_CONTENT = "This is a test message to see if it works with Flask."
-ARTICLE_AUTHOR = "Aedan Lawrence"
-ARTICLE_PUBLISHER = "Financial Times"
-
-FULL_ARTICLE = str.encode(ARTICLE_TITLE) + str.encode(ARTICLE_CONTENT) + str.encode(ARTICLE_AUTHOR)
-PRIVATE_KEY = read_key(ARTICLE_PUBLISHER)
-SIGNATURE = sign_news(PRIVATE_KEY, FULL_ARTICLE)
 
 if __name__ == '__main__':
     app.run()
--- Python Flask Enviroment/load_database.py
+++ Python Flask Enviroment/load_database.py
@@ -1,13 +1,11 @@
 """Used to populate the database as and when needed."""
-from database import db, PrivateKey, NewsArticle
+from app import db, PrivateKey
 
 db.create_all()
 bbc = PrivateKey(publisher = 'BBC', private_key = '-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIAJNcYYp63fPZUWTy+tWosDfu8De9coRoofeeDOU2J3joAcGBSuBBAAK\noUQDQgAEp1h8PZuoY3oD83bC9I/kxj/SVfLK41e0FlX/sLM8F9/jetRTtgC4WM89\n23rJxQ9p3s79CL5+xha3oOfpriCAmA==\n-----END EC PRIVATE KEY-----')
 ft = PrivateKey(publisher = 'Financial Times', private_key = '-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIKtFHMrBzAhMQiNFW/kRpZVZSQGEj3QeApyU1d7Vq0k3oAcGBSuBBAAK\noUQDQgAEMwzhviezqpfVc+OBoJOlMzVkpJUrqq7TBo7+ALReehlT1s0SYKbR+Kwe\nOYUWhOpCdk9QWSdW7xxI1kBRVaiu3w==\n-----END EC PRIVATE KEY-----')
+guardian = PrivateKey(publisher = 'Guardian', private_key = '-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIMMTKdk0xZXDoRVpV7y2HJz8p1syzt4IYb3EdpHA0Q5JoAcGBSuBBAAK\noUQDQgAELkQAGX3lFaxZU/2l8NdPp0GK9Cpgi5UaVYfoPEookXzgeWjVlX+tZIp8\ngqzuXERbPeX5opbXkTaQ/dvvY2UCJg==\n-----END EC PRIVATE KEY-----')
 db.session.add(bbc)
 db.session.add(ft)
-article1 = NewsArticle(publisher = 'BBC', author = 'Aedan Lawrence', title = 'BREAKING NEWS: Royal Holloway is a University', content = 'This is a test message to see if it works with Flask.')
-article2 = NewsArticle(publisher = 'Financial Times', author = 'Aedan Lawrence', title = 'BREAKING NEWS: I live in Sussex', content = 'This is a test message to see if it works with Flask.')
-db.session.add(article1)
-db.session.add(article2)
+db.session.add(guardian)
 db.session.commit()
